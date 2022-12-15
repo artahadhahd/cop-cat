811075a168acf569b962eb1c0cd60d1541127e5d
artahadhahd <116200657+artahadhahd@users.noreply.github.com>
2022-12-15 19:42:40 +0100

    Added main.cpp - initial version

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+int main(int argc, char **argv){
+	if (argc != 2){
+		std::cout << "Invalid quantity of arguments: " << argc - 1 << "\nType \"cop -h\" for more information." << std::endl;
+		exit(1);
+	}
+
+	std::fstream file;
+	if (!strcmp(argv[1], "-h")){
+		std::cout << "Usage: cop <file>\nPrints contents of a file\nconcat -h: display this help message" << std::endl;
+		exit(0);
+	}
+
+	unsigned int line_number = 0;
+	file.open(argv[1], std::ios::in);
+	if (file.is_open()){
+		std::string line;
+		while(std::getline(file, line)){
+			line_number++;
+			if (line_number < 10)
+				std::cout << "    " << line_number << " | " << line << "\n";
+			else if (line_number < 100)
+				std::cout << "   " << line_number << " | " << line << "\n";
+			else if (line_number < 1000)
+				std::cout << "  " << line_number << " | " << line << "\n";
+			else if (line_number < 10000)
+				std::cout << " " << line_number << " | " << line << "\n";
+		}
+		file.close();
+	}
+	else {
+		std::cout << "cop: file \"" << argv[1] << "\" was not found.\n";
+		exit(1);
+	}
+}
